1d1f15f2cabf213ce86b5b798dbc0023a4676d2e
maaz157 <153020978+maaz157@users.noreply.github.com>
2025-04-21 00:50:04 +0500

    Add files via upload

---
+++ Assignments 00 to 05/00_intro_python/how_old_are_they.py
@@ -0,0 +1,15 @@
+# Farhan, Maaz, Saad, Daniyal, aur Faraz  dost hain.
+def main():
+   farhan = 18
+   maaz = -1 + farhan
+   saad = 0 + maaz
+   daniyal = farhan
+   faraz = saad
+
+   print(f"Farhan is {farhan}")
+   print(f"Maaz is {maaz}")
+   print(f"Saad is {saad}")
+   print(f"Daniyal is {daniyal}")
+   print(f"Faraz is {faraz}")
+
+main()
